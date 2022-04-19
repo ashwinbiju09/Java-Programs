41594035325766ca41280118a5218094b5ac8202
Ashwin K Biju <81153696+ashwinbiju09@users.noreply.github.com>
2022-04-19 21:49:36 +0530

    Add files via upload

---
+++ Java Loops I/Solution.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Solution{
+    public static void main(String[] args){
+        Scanner obj = new Scanner(System.in);
+        int n = obj.nextInt();
+        for (int i=1 ; i<=10 ; i++){
+            System.out.println(n+" x "+i+" = "+n*i);
+        }
+    }
+}
